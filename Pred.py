81cd41f283e16f7ae88283109cb8e9bd6a4f52f0
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-07-06 18:01:13 +0530

    Large Model Prediction

--- Pred.py
+++ Pred.py
@@ -1,7 +1,7 @@
 import os
 from ultralytics import YOLO
 path=r"./prediction/images"
-model=YOLO('Model1.pt')
+model=YOLO('Model2(Large).pt')
 for files in os.listdir(path):
   print(files)
   model.predict(source=f"{path}/{files}",show=True,save=True)

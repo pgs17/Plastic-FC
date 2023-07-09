529847c8712bf657030fc4e75f95f3a3bddb04b4
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-07-09 16:30:33 +0530

    Video prediction

--- frames.py
+++ frames.py
@@ -1,17 +1,43 @@
+import os
+
+from ultralytics import YOLO
 import cv2
 
-path="C:\\Users\\Lenovo\\Documents\\rolex.mp4"
-vid=cv2.videocapture(path)
 
-while (vid.isOpened()):
-    ret,frame=vid.read()
-    frame=cv2.resize(frame,(640,640))
-    if ret== True :
-        #type your code here 
-        cv2.imshow("result",frame)
-        k=cv2.waitKey(1)
-        if k==ord('q'):
-            break
+VIDEOS_DIR = os.path.join('.', 'videos')
 
-vid.release()
+# input videp path (Kindly Enter Your Own path)
+video_path = input("Kindly Enter the Video path")
+
+# output video path
+video_path_out = '{}_out.mp4'.format(video_path)
+
+cap = cv2.VideoCapture(video_path)
+ret, video_frame = cap.read()
+H, W, _ = video_frame.shape
+out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
+
+
+# Load a model
+model = YOLO('Model2(Large).pt')  # load a custom model
+
+threshold = 0.35
+
+while ret:
+
+    results = model(video_frame)[0]
+
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+
+        if score > threshold:
+            cv2.rectangle(video_frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+            cv2.putText(video_frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+
+    out.write(video_frame)
+    ret, video_frame = cap.read()
+
+cap.release()
+out.release()
 cv2.destroyAllWindows()
